f7c39887fac9afeb795a8e982175116c0c643986
Lital Hen <litalh2702@gmail.com>
2021-04-29 01:02:42 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import GalleryCard from "./component/Gallery-view";
 import Actor from "./data-models/Actor";
 
 function App() {
-  const actor1= new Actor ('lital','hen','1989-02-27');
-  const actor2= new Actor ('hen','l','1989-05-27');
+  const actor1= new Actor ('lital','hen','1989-02-27','https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UX214_CR0,0,214,317_AL_.jpg','https://www.imdb.com/name/nm0425005/');
+  const actor2= new Actor ('viki','sau','1989-05-27');
   const actorArr=[actor1, actor2]
   return (
     <div>
--- src/component/gallery-view.js
+++ src/component/gallery-view.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 
 class GalleryView extends React.Component{
     constructor(props){
         super(props);
+   
+    this.state={
+        filterChar:'i'
     }
-
+}
     render(){
-        const actorArr= this.props.actorData.map((actor) => {
+        const actorArr= this.props.actorData.filter(actor=>(`${actor.fname} ${actor.lname}`).includes(this.state.filterChar))
+        .map((actor) => {
 
-            return( <div className="card">
-                        {actor.image}
-                        <div className="card-content">
-                        {actor.fname}
-                        {actor.lname}                     
-                        {actor.imdbLink}
-                        {actor.calcAge()}
-                        </div>
-                    </div>
+            return(
+                 <Card style={{ width: '18rem' }} key={actor.id} >
+                     <Card.Body>
+                        <Card.Img src={actor.image}/>
+                        <Card.Title>{`${actor.fname} ${actor.lname}`} </Card.Title>
+                        <Card.Text>{` ${actor.calcAge()}`} </Card.Text> 
+                        <small className="text-muted" ><Card.Link> <a href={actor.imdbLink}> IMDB</a> </Card.Link> </small>
+                        </Card.Body>
+                </Card>
             )
         })
         return(
